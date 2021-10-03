b8fff094b5afe29d066797364320bebd57628a4a
Film Bukorree Madthing <ffeem59@gmail.com>
2021-10-03 14:03:48 +0700

    lab7

--- LAB7/2/index.php
+++ LAB7/2/index.php
@@ -10,36 +10,157 @@
     <link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
+
     <style>
         body {
             font-family: 'Mitr', sans-serif;
+        }
+
+        .fontw {
+            color: white;
+        }
+
+
+        .sun {
+            background-color: #FF5C58;
+
+        }
+
+        .mon {
+            background-color: #FFEF78;
+
+        }
+
+        .tue {
+            background-color: #FFDEFA;
+
+        }
+
+        .wed {
+            background-color: #B1E693;
+
+        }
+
+        .thu {
+            background-color: #FFB085;
+
+        }
+
+        .fri {
+            background-color: cyan;
+
+        }
+
+        .sat {
+            background-color: purple;
+
         }
     </style>
 </head>
 
 <body>
-    <h1> 63070234 </h1>
-    <?php
-    $d1 = strtotime("April 01");
-    $d2 = ceil(($d1 - time()) / 60 / 60 / 24);
-    echo "There are " . $d2 . " days until 4th of July.</br>";
-    echo date('l', $d1);
 
-    $DayOfMonth = array( // Key => Value
-        "Januaray" => 31,
-        "February" => 28,
-        "March" => 31,
-        "April" => 30,
-        "May" => 31,
-        "June" => 30,
-        "July" => 31,
-        "August" => 31,
-        "September" => 30,
-        "October" => 31,
-        "November" => 30,
-        "December" => 31
-    );
-    ?>
+
+
+    <div class="container box">
+        <div class="col text-center pt-5">
+            <h1>
+                <?php
+                // รหัสนศ.
+                $id = 63070234;
+                $id = $id % 10;
+
+                $d1 = strtotime("2021/" . $id . "/1");
+                $Months = [
+                    "Januaray",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July",
+                    "August",
+                    "September",
+                    "October",
+                    "November",
+                    "December"
+                ];
+                $DayOfMonth = array(
+                    "Januaray" => 31,
+                    "February" => 28,
+                    "March" => 31,
+                    "April" => 30,
+                    "May" => 31,
+                    "June" => 30,
+                    "July" => 31,
+                    "August" => 31,
+                    "September" => 30,
+                    "October" => 31,
+                    "November" => 30,
+                    "December" => 31
+                );
+                $thismonts = $Months[$id - 1];
+                echo  $Months[$id - 1];
+                $wd = date("l", $d1);
+                echo " 2021 start at " . $wd;
+                $bd = array(
+                    "Sunday" => 0,
+                    "Monday" => 1,
+                    "Tuesday" => 2,
+                    "Wednesday" => 3,
+                    "Thursday" => 4,
+                    "Friday" => 5,
+                    "Saturday" => 6,
+                )
+                ?>
+            </h1>
+        </div>
+
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <?php
+                    $weekday = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+                    foreach ($weekday as $day) {
+                        echo "<th scope=\"col\" class=\"\"><div class=\"col rounded m-4 fontw text-center text-uppercase pt-4 $day\"><h1>$day</h1></div></th>";
+                    }
+                    ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $w = $bd[$wd];
+                echo "<tr>";
+                for ($i = 0; $i < $w; $i++) {
+                    echo "<td></td>";
+                }
+                for ($i = 1; $i <= $DayOfMonth[$thismonts]; $i++) {
+
+                    if ($w == 0) {
+                        echo "<tr>";
+                    }
+
+
+                    echo "<td class=\" text-center\">$i</td>";
+                    if ($w == 6) {
+                        echo "</tr>";
+                    }
+                    $w++;
+                    $w = $w % 7;
+                }
+                for ($i = 0; $i < 7 - $w; $i++) {
+                    echo "<td></td>";
+                }
+
+                ?>
+
+            </tbody>
+        </table>
+    </div>
+
+
+
+
 </body>
 
 </html>
---
+++ LAB7/3/index.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EX3</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    <style>
+        body {
+            font-family: 'Mitr', sans-serif;
+        }
+
+        .box {
+            /* width: 600;
+            height: 750; */
+            background-color: #555;
+            border-radius: 40px;
+
+        }
+
+        .color {
+            background-color: aquamarine;
+        }
+
+        .flexbox {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+
+        }
+
+
+
+        .err {
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    // define variables and set to empty values
+    $nameErr = $lnameErr = $addressErr = $tumErr = $idErr = $aumErr = $provinceErr = $zipErr = $phoneErr = "";
+    $name = $lname = $address = $tum = $id = $aum = $province = $zip = $phone = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "err";
+        } else {
+            $name = test_input($_POST["name"]);
+        }
+
+        if (empty($_POST["lname"])) {
+            $lnameErr = "err";
+        } else {
+            $lname = test_input($_POST["lname"]);
+        }
+
+        if (empty($_POST["id"])) {
+            $idErr = "err";
+        } else {
+            $id = test_input($_POST["id"]);
+        }
+        if (empty($_POST["address"])) {
+            $addressErr = "err";
+        } else {
+            $address = test_input($_POST["address"]);
+        }
+
+        if (empty($_POST["tum"])) {
+            $tumErr = "err";
+        } else {
+            $tum = test_input($_POST["tum"]);
+        }
+
+        if (empty($_POST["aum"])) {
+            $aumErr = "err";
+        } else {
+            $aum = test_input($_POST["aum"]);
+        }
+        if (empty($_POST["province"])) {
+            $provinceErr = "err";
+        } else {
+            $province = test_input($_POST["province"]);
+        }
+        if (empty($_POST["zip"])) {
+            $zipErr = "err";
+        } else {
+            $zip = test_input($_POST["zip"]);
+        }
+        if (empty($_POST["phone"])) {
+            $phoneErr = "err";
+        } else {
+            $phone = test_input($_POST["phone"]);
+        }
+    }
+
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+    <div class="flexbox">
+        <div class="m-5 text-white p-4 box">
+            <h1>Form</h1>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF"]); ?>">
+                <div class="form-group mb-3 <?php echo $idErr ?>">
+                    <label for="inputAddress">หมายเลขบัตรประชาชน</label>
+                    <input type="text" name="id" class="form-control" id="inputid" max="13" placeholder="x xxxx xxxxx xx x">
+                </div>
+                <div class="row">
+                    <div class="form-group col-md-6 mb-3 <?php echo $nameErr ?>">
+                        <label for="inputname">ชื่อ</label>
+                        <input type="text" name="name" class="form-control" id="inputname" placeholder="Name">
+                    </div>
+                    <div class="form-group col-md-6 mb-3 <?php echo $lnameErr ?>">
+                        <label for="inputlname">นามสกุล</label>
+                        <input type="text" name="lname" class="form-control" id="inputlname" placeholder="Lastname">
+                    </div>
+                </div>
+                <div class="form-group mb-3 <?php echo $addressErr ?>">
+                    <label for="inputAddress">ที่อยู่</label>
+                    <input type="text" name="address" class="form-control" id="inputAddress" placeholder="Address">
+                </div>
+                <div class="form-group mb-3 <?php echo $tumErr ?>">
+                    <label for="inputAddress2">ตำบล</label>
+                    <input type="text" name="tum" class="form-control" id="inputAddress2" placeholder="">
+                </div>
+                <div class="row">
+                    <div class="form-group col-md-6 mb-3 <?php echo $aumErr ?>">
+                        <label for="inputCity">อำเถอ</label>
+                        <input type="text" name="aum" class="form-control" id="inputCity">
+                    </div>
+                    <div class="form-group col-md-4 mb-3 <?php echo $provinceErr ?>">
+                        <label for="inputState">จังหวัด</label>
+                        <input type="text" name="province" class="form-control" id="inputState">
+                    </div>
+                    <div class="form-group col-md-2 mb-3 <?php echo $zipErr ?>">
+                        <label for="inputZip">รหัสไปรษณีย์</label>
+                        <input type="text" name="zip" class="form-control" id="inputZip">
+                    </div>
+                </div>
+                <div class="form-group mb-3 <?php echo $phoneErr ?>">
+                    <label for="inputAddress">หมายเลขโทรศัพท์</label>
+                    <input type="text" name="phone" class="form-control" id="inputid" max="13" min="13" placeholder="Phone Number">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
+        </div>
+    </div>
+    <?php
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    echo $lname;
+    echo "<br>";
+    echo $id;
+    echo "<br>";
+    echo $address;
+    echo "<br>";
+    echo $tum;
+    echo "<br>";
+    echo $aum;
+    echo "<br>";
+    echo $province;
+    echo "<br>";
+    echo $phone;
+
+    echo $nameErr . $lnameErr . $addressErr . $tumErr . $idErr . $aumErr . $provinceErr . $zipErr . $phoneErr
+
+
+    ?>
+
+</body>
+
+</html>
